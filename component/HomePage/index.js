e13a6c06106a5207c9bf103e6bd7b4562129159e
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-03-18 15:24:40 +0530

    Add files via upload

--- component/HomePage/index.js
+++ component/HomePage/index.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import UserTable from "../UserTable/index";
 import SearchInput from "../SearchAndPagiAndAddUser/index";
 import UserAddForm from "../UserAddForm/index";
-import DM from "../DelForm/index";
-import { getUsers, deleteUsers, addUsers } from "../../ApiServiceProvider";
+import {
+  getUsersAPI,
+  deleteUsersAPI,
+  addUsersAPI
+} from "../../ApiServiceProvider";
 
 // CardVaultList
 // CardVaultAddEdit isVisible, cardVaultId={1}
-var id;
 export default class HomePage extends Component {
   state = {
     isDialogVisible: false,
@@ -17,28 +19,14 @@ export default class HomePage extends Component {
     search: "",
     users: [],
     totalItems: "",
-    user: {
-      name: "",
-      contect: "",
-      bankname: "",
-      cardnumber: "",
-      Email: ""
-    },
-    fieldError: {
-      nameError: "",
-      contectError: "",
-      banknameError: "",
-      cardnumberError: ""
-    },
     activePage: 1,
     pageLimit: 5,
-    isdisable: false,
+    isDisable: false,
     sortBy: "name",
-    sortOrder: "asc",
-    deletepage: false
+    sortOrder: "asc"
   };
-  getUserHelper = () => {
-    getUsers(this.state, true).then(fetchusers => {
+  getUsers = () => {
+    getUsersAPI(this.state, true).then(fetchusers => {
       this.setState({
         users: fetchusers.items,
         totalItems: fetchusers.total
@@ -46,58 +34,63 @@ export default class HomePage extends Component {
     });
   };
   componentDidMount() {
-    this.getUserHelper();
+    this.getUsers();
   }
-  handlerPageChange = (page, limit) => {
+  pageChangeHandler = (page, limit) => {
     this.setState(
       {
         activePage: page,
         pageLimit: limit
       },
       () => {
-        this.getUserDetails();
+        this.getUsers();
       }
     );
   };
-  handlerSearch = event => {
+  onChangeHandler = event => {
     const {
       target: { name, value }
     } = event;
     this.setState({ [name]: value, activePage: 1 }, () => {
-      this.getUserHelper();
+      this.getUsers();
     });
   };
-  handlerUserDetailChange = event => {
+  userDetailChangeHandler = event => {
     const {
       target: { name, value }
     } = event;
+    let input = `${[name]}`;
     let field = `${[name]}Error`;
-    this.setState(
-      {
-        errorDialog: false,
-        fieldError: {
-          ...this.state.fieldError,
-          [field]: ""
-        },
-        user: {
-          ...this.state.user,
-          [name]: value
-        }
-      },
-      () => {
-        this.validate();
+    this.setState({
+      user: {
+        ...this.state.user,
+        [name]: value
       }
-    );
+    });
+    //  if (input === "name") {
+    this.setState({
+      errorDialog: true,
+      fieldError: {
+        ...this.state.fieldError,
+        [field]:
+          value.length === 0
+            ? "Required!"
+            : // : !value.match("^[A-Za-z]*$")
+              // ? "Invalid Name!"
+              ""
+      }
+    });
+    // }
   };
-  handlerNewUserButtonClick = () => {
+  newUserButtonClickHandler = () => {
     this.setState(state => ({
       ...state,
       isAdd: true,
       isDialogVisible: !state.isDialogVisible,
-      user: { id: "", name: "", contect: "", bankname: "", cardnumber: "" }
+      user: { id: "" }
     }));
   };
-  handlerEditUserClick = userobj => {
+  editUserClickHandler = userobj => {
     this.setState(state => ({
       ...state,
       user: userobj,
@@ -105,54 +98,40 @@ export default class HomePage extends Component {
       isDialogVisible: !state.isDialogVisible
     }));
   };
-  handlerDeleteUserClick = userid => {
-    this.setState(state => ({
-      ...state,
-      deletepage: true
-    }));
-     id = userid;
-    
-  
+  deleteUserClickHandler = userid => {
+    if (window.confirm("Are you sure?") === true) {
+      deleteUsersAPI(userid);
+    }
+    this.getUsers();
   };
-  handlerdeleteUser=() =>{
-    this.setState(state => ({
-      ...state,
-      deletepage: false
-    }));
-    deleteUsers(id);
-    this.getUserDetails();
-  }
-
   validate = () => {
     let nameError = "";
     let contectError = "";
     let banknameError = "";
-    let cardnumberError = "";
-    !this.state.user.name ? (nameError = "Name Required!") : (nameError = "");
+    let emailError = "";
+    !this.state.user.name ? (nameError = "Required!") : (nameError = "");
     if (this.state.user.name && !this.state.user.name.match("^[A-Za-z]*$")) {
-      nameError = "Firstname and Lastname Required!";
+      nameError = "Invalid Name!";
     }
     !this.state.user.contect
-      ? (contectError = "Contect Number Required!")
+      ? (contectError = "Required!")
       : (contectError = "");
     !this.state.user.bankname
-      ? (banknameError = "Bank Name Required!")
+      ? (banknameError = "Required!")
       : (banknameError = "");
     if (this.state.user.contect && this.state.user.contect.length !== 10) {
       contectError = "Invalid Contact Number!Length must be 10.";
     }
-    !this.state.user.Email
-      ? (cardnumberError = "Email Required!")
-      : (cardnumberError = "");
+    !this.state.user.Email ? (emailError = "Required!") : (emailError = "");
     if (
       this.state.user.Email &&
       !this.state.user.Email.match(
         "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$"
       )
     ) {
-      cardnumberError = "Invalid Email!";
+      emailError = "Invalid Email!";
     }
-    if (nameError || contectError || banknameError || cardnumberError) {
+    if (nameError || contectError || banknameError || emailError) {
       this.setState({
         errorDialog: true,
         fieldError: {
@@ -160,45 +139,48 @@ export default class HomePage extends Component {
           nameError,
           contectError,
           banknameError,
-          cardnumberError
+          emailError
         }
       });
       return false;
     }
     return true;
   };
-  handlerSubmit = newusersdetails => {
+  submitHandler = newusersdetails => {
     var isValid = this.validate();
     if (isValid) {
-      this.setState(state => ({
-        isAdd: !state.isAdd,
-        isDialogVisible: !state.isDialogVisible,
-        isEdit: !state.isEdit,
-        fieldError: {
-          ...this.state.fieldError
-        },
-        ...state,
-        user: newusersdetails
-      }));
-      addUsers(this.state.user, this.state.isEdit).then(fetchusers =>
-        this.setState({
-          users: fetchusers,
-          totalItems: this.state.users
-        })
+      addUsersAPI(this.state.user, this.state.isEdit).then(
+        this.setState(state => ({
+          isAdd: false,
+          isEdit: false,
+          isDialogVisible: false,
+          errorDialog: false,
+          fieldError: {
+            ...this.state.fieldError
+          },
+          ...state,
+          user: newusersdetails
+        }))
       );
-      this.getUserDetails();
     }
+    this.getUsers();
   };
-  handlerClose = () => {
+  closeHandler = () => {
     this.setState({
       isAdd: false,
       isEdit: false,
       isDialogVisible: false,
       errorDialog: false,
-      deletepage: false
+      fieldError: {
+        ...this.state.fieldError,
+        nameError: "",
+        contectError: "",
+        banknameError: "",
+        emailError: ""
+      }
     });
   };
-  handlerSorting = (attrib, order) => {
+  sortingHandler = (attrib, order) => {
     this.setState(
       {
         ...this.state,
@@ -206,43 +188,28 @@ export default class HomePage extends Component {
         sortOrder: order,
         activePage: 1
       },
-      () => this.getUserDetails()
+      () => this.getUsers()
     );
   };
-  getUserDetails = () => {
-    getUsers(this.state).then(fetchusers =>
-      this.setState({
-        users: fetchusers
-      })
-    );
-  };
-  handlerClear = () => {
+  clearHandler = () => {
     this.setState({ ...this.state, search: "" }, () => {
-      this.getUserHelper();
+      this.getUsers();
     });
   };
 
   render() {
-    var totalPage;
-    var rows = [];
-    this.state.totalItems % this.state.pageLimit === 0
-      ? (totalPage = this.state.totalItems / this.state.pageLimit)
-      : (totalPage = this.state.totalItems / this.state.pageLimit + 1);
-    for (let i = 1; i <= totalPage; i++) {
-      rows.push(i);
-    }
     return (
       <React.Fragment>
         <hr />
         <SearchInput
-          items={rows}
+          items={Math.ceil(this.state.totalItems / this.state.pageLimit)}
           limit={this.state.pageLimit}
           searchKey={this.state.search}
           activePage={this.state.activePage}
-          onChange={this.handlerSearch}
-          onClear={this.handlerClear}
-          onAddUserClick={this.handlerNewUserButtonClick}
-          onPageChange={this.handlerPageChange}
+          onChange={this.onChangeHandler}
+          onClear={this.clearHandler}
+          onAddUserClick={this.newUserButtonClickHandler}
+          onPageChange={this.pageChangeHandler}
         />
         <hr />
         {this.state.isAdd && (
@@ -251,30 +218,29 @@ export default class HomePage extends Component {
             addform={this.state.isAdd}
             errordialog={this.state.errorDialog}
             errors={this.state.fieldError}
-            onChange={this.handlerUserDetailChange}
-            onClick={this.handlerSubmit}
-            onCloseClick={this.handlerClose}
+            onChange={this.userDetailChangeHandler}
+            onClick={this.submitHandler}
+            onCloseClick={this.closeHandler}
           />
         )}
-        {this.state.deletepage && <DM id={id} onConfirmClick={this.handlerdeleteUser} onCloseClick={this.handlerClose} />}
         {this.state.isEdit && (
           <UserAddForm
             nameKey={this.state.user}
             addform={this.state.isAdd}
             errordialog={this.state.errorDialog}
             errors={this.state.fieldError}
-            onChange={this.handlerUserDetailChange}
-            onClick={this.handlerSubmit}
-            onCloseClick={this.handlerClose}
+            onChange={this.userDetailChangeHandler}
+            onClick={this.submitHandler}
+            onCloseClick={this.closeHandler}
           />
         )}
         <UserTable
           user={this.state.users}
-          isdisable={this.state.isdisable}
-          onEditUserClick={this.handlerEditUserClick}
+          isDisable={this.state.isDisable}
+          onEditUserClick={this.editUserClickHandler}
           onPaymentCall={this.handlerPayment}
-          onDeleteUserClick={this.handlerDeleteUserClick}
-          onSortingClick={this.handlerSorting}
+          onDeleteUserClick={this.deleteUserClickHandler}
+          onSortingClick={this.sortingHandler}
         />
       </React.Fragment>
     );
