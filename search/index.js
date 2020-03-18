34e57068b1af493989249344d12c4bb587fcefce
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-03-18 15:25:12 +0530

    Add files via upload

--- search/index.js
+++ search/index.js
@@ -1,106 +1,133 @@
 import React from "react";
 
 import * as Bootstrap from "react-bootstrap";
-
-
+import * as Icons from "react-bootstrap-icons";
 import { FormControl } from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
 
 const SearchInput = props => {
   return (
     <React.Fragment>
       <Bootstrap.Container>
         <Bootstrap.Row>
-          <Bootstrap.Col xs="3">
-          <h3 style={{ color: "black" }}>Contact Vault</h3>
+          <Bootstrap.Col xs="15">
+            <h3 >Contact Vault</h3>
           </Bootstrap.Col>
           <Bootstrap.Col></Bootstrap.Col>
           <Bootstrap.Col xs="15">
-            {" "}
             <Bootstrap.Button
               onClick={props.onAddUserClick}
-              size="sm"
               variant="secondary"
             >
               Request New Card
             </Bootstrap.Button>
-            
           </Bootstrap.Col>
         </Bootstrap.Row>
-        <hr/>
+        <hr />
         <Bootstrap.Row>
-          <Bootstrap.Col ><Bootstrap.InputGroup>
-              <div class="input-group mb-3">               
+          <Bootstrap.Col xs="15">
+            <Bootstrap.InputGroup>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="basic-addon1">
+                    {" "}
+                    <Icons.Search></Icons.Search>
+                  </span>
+                </div>
                 <FormControl
                   placeholder="Search"
                   type="search"
                   value={props.searchKey}
                   onChange={props.onChange}
                   name="search"
-                  required
                 ></FormControl>
-                <div class="input-group-append"></div>
               </div>
-            </Bootstrap.InputGroup></Bootstrap.Col>
+            </Bootstrap.InputGroup>
+          </Bootstrap.Col>
           <Bootstrap.Col></Bootstrap.Col>
-          <Bootstrap.Col>
-            <div class="datatables_info">
+          <Bootstrap.Col xs="15">
+            <div class="dropdown-select">
               <span>
-                Showing:
-                <select
-                  class="custom-select custom-select-sm "
-                 style={{width:"40%"}}
-                  as="select"
-                  size="sm"
-                  name="pageLimit"
-                  value={props.limit}
-                  onChange={props.onChange}
+                Showing :
+                <Dropdown
+                  onSelect={(ekey, e) =>
+                    props.onChange({
+                      target: {
+                        value: e.currentTarget.innerText,
+                        name: "pageLimit"
+                      }
+                    })
+                  }
                 >
-                  <option class="dropdown-item">5</option>
-                  <option class="dropdown-item">10</option>
-                  <option class="dropdown-item">20</option>
-                  <option class="dropdown-item">50</option>
-                </select>{" "}
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {props.limit}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item>5</Dropdown.Item>
+                    <Dropdown.Item>10</Dropdown.Item>
+                    <Dropdown.Item>15</Dropdown.Item>
+                    <Dropdown.Item>20</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                
               </span>
-              Entries
             </div>
           </Bootstrap.Col>
-          <Bootstrap.Col xs="4">
-            {" "}
+          <Bootstrap.Col xs="15">
             <nav aria-label="...">
               <ul class="pagination">
-                <Bootstrap.Col xs="3">
-                  {props.activePage !== 1 && (
+                <Bootstrap.Col>
+                  {
                     <li
-                      class="page-item"
+                      class={
+                        props.activePage !== 1
+                          ? "page-item"
+                          : "page-item disabled"
+                      }
                       onClick={() =>
-                        props.onPageChange(props.activePage - 1, props.limit)
+                        props.activePage !== 1
+                          ? props.onPageChange(
+                              props.activePage - 1,
+                              props.limit
+                            )
+                          : null
                       }
                     >
                       <a class="page-link" href="#">
                         Prev.
                       </a>
                     </li>
-                  )}
+                  }
                 </Bootstrap.Col>
                 <li class="page-item">
                   <h5 class="form-control ">
-                    {props.activePage} of {props.items.length}
+                    {props.activePage} of {props.items}
                   </h5>
                 </li>
-                <Bootstrap.Col xs="3">
+                <Bootstrap.Col>
                   {" "}
-                  {props.activePage < props.items.length && (
+                  {
                     <li
-                      class="page-item "
+                      class={
+                        props.activePage < props.items
+                          ? "page-item"
+                          : "page-item disabled"
+                      }
                       onClick={() =>
-                        props.onPageChange(props.activePage + 1, props.limit)
+                        props.activePage < props.items
+                          ? props.onPageChange(
+                              props.activePage + 1,
+                              props.limit
+                            )
+                          : null
                       }
                     >
                       <a class="page-link" href="#">
                         Next
                       </a>
                     </li>
-                  )}
+                  }
                 </Bootstrap.Col>
               </ul>
             </nav>
@@ -111,21 +138,3 @@ const SearchInput = props => {
   );
 };
 export default SearchInput;
-// {props.items.map((i = 0) => {
-//   return (
-//     console.log(props.limit),
-//     (
-//       <button
-//         key={i}
-//         onClick={() => props.onPageChange(i, props.limit)}
-//         style={{
-//           backgroundColor: `${
-//             props.activePage === i ? " rgb(224, 222, 243)" : ""
-//           }`
-//         }}
-//       >
-//         {++pagenumber}
-//       </button>
-//     )
-//   );
-// })}
