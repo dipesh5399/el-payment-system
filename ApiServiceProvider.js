181cc3125c44df8836513b6e566d9f24766a3003
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-03-19 18:48:51 +0530

    Add files via upload

--- ApiServiceProvider.js
+++ ApiServiceProvider.js
@@ -1,19 +1,24 @@
-export var addUsers = function(addUserDetails, isEdit) {
-  return fetch(`http://localhost:3005/users/${addUserDetails.id}`, {
-    method: isEdit ? "PATCH" : "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: addUserDetails.name,
-      contect: addUserDetails.contect,
-      bankname: addUserDetails.bankname,
-      cardnumber: addUserDetails.cardnumber
-    })
-  });
+export var addUsersAPI = function(addUserDetails, getMethod) {
+  return fetch(
+    addUserDetails.id
+      ? `http://localhost:3005/users/${addUserDetails.id}`
+      : `http://localhost:3005/users`,
+    {
+      method: getMethod ? "PATCH" : "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: addUserDetails.name,
+        contect: addUserDetails.contect,
+        bankname: addUserDetails.bankname,
+        Email: addUserDetails.Email
+      })
+    }
+  );
 };
 
-export var getUsers = function(Param, isGetTotalCount) {
+export var getUsersAPI = function(Param, isGetTotalCount) {
   return fetch(
     Param.search
       ? `http://localhost:3005/users?q=${Param.search}&_sort=${
@@ -27,7 +32,7 @@ export var getUsers = function(Param, isGetTotalCount) {
           Param.activePage ? Param.activePage : 1
         }&_limit=${Param.pageLimit ? Param.pageLimit : 5}`,
     {
-      headers: { X_Total_Count: getUsers.length }
+      headers: { X_Total_Count: getUsersAPI.length }
     }
   ).then(response => {
     if (isGetTotalCount) {
@@ -42,10 +47,20 @@ export var getUsers = function(Param, isGetTotalCount) {
   });
 };
 
-export var deleteUsers = function(userobj) {
+export var deleteUsersAPI = function(userobj) {
   return fetch(`http://localhost:3005/users/${userobj}`, {
     method: "DELETE"
   }).then(response => {
     return response.json();
+  });
+};
+
+export var getUserByIDAPI = function(userid) {
+  return fetch(`http://localhost:3005/users/${userid}`).then(response => {
+    return response.json().then(items => {
+      return {
+        items
+      };
+    });
   });
 };
