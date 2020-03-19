5398c204d15046380f7cdc8c40af715e263aaaad
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-03-19 18:50:20 +0530

    Add files via upload

--- UserTable/index.js
+++ UserTable/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "../UserTable/userTable.css";
-
 import * as BT from "react-bootstrap";
 import * as Icons from "react-bootstrap-icons";
 
@@ -10,60 +9,13 @@ const UserTable = props => {
   var count = 0;
 
   return props.user.length === 0 ? (
-    <div align="center" >
+    <div align="center">
       <h4>No Data Available.</h4>
     </div>
   ) : (
     <BT.Container>
       <div class="limiter">
         <div class="table100 ver3 m-b-110 ">
-          {/* <div class="table100-head">
-          <table class="table table-hover table-borderless">
-            <thead>
-              <tr>
-                <th>Sr. No.</th>
-                <th>
-                  NAME
-                  {"    "}
-                  <Icons.ArrowUp
-                    size={25}
-                    onClick={() => props.onSortingClick("name", "asc")}
-                  ></Icons.ArrowUp>
-                  <Icons.ArrowDown
-                    size={25}
-                    onClick={() => props.onSortingClick("name", "desc")}
-                  ></Icons.ArrowDown>
-                </th>
-                <th>
-                  Phone
-                  <Icons.ArrowUp
-                    size={25}
-                    onClick={() => props.onSortingClick("contect", "asc")}
-                  ></Icons.ArrowUp>
-                  <Icons.ArrowDown
-                    size={25}
-                    onClick={() => props.onSortingClick("contect", "desc")}
-                  ></Icons.ArrowDown>
-                </th>
-                <th>
-                  BANK
-                  <Icons.ArrowUp
-                    size={25}
-                    onClick={() => props.onSortingClick("bankname", "asc")}
-                  ></Icons.ArrowUp>
-                  <Icons.ArrowDown
-                    size={25}
-                    onClick={() => props.onSortingClick("bankname", "desc")}
-                  ></Icons.ArrowDown>
-                </th>
-                <th>User Email ID</th>
-                <th>Payment</th>
-                <th>Profile</th>
-              </tr>
-            </thead>
-          </table>
-        </div> */}
-
           <div class="table100-body ">
             <table
               style={{ tableLayout: "fixed" }}
@@ -74,37 +26,48 @@ const UserTable = props => {
                   <th>Sr. No.</th>
                   <th>
                     NAME
-                    {"    "}
-                    <Icons.ArrowUp
-                      size={25}
-                      onClick={() => props.onSortingClick("name", "asc")}
-                    ></Icons.ArrowUp>
-                    <Icons.ArrowDown
-                      size={25}
-                      onClick={() => props.onSortingClick("name", "desc")}
-                    ></Icons.ArrowDown>
+                    <a href="#">
+                      <Icons.ArrowUp
+                        size={25}
+                        onClick={() => props.onSortingClick("name", "asc")}
+                      ></Icons.ArrowUp>
+                    </a>
+                    <a href="#">
+                      <Icons.ArrowDown
+                        size={25}
+                        onClick={() => props.onSortingClick("name", "desc")}
+                      ></Icons.ArrowDown>
+                    </a>
                   </th>
                   <th>
                     Phone
-                    <Icons.ArrowUp
-                      size={25}
-                      onClick={() => props.onSortingClick("contect", "asc")}
-                    ></Icons.ArrowUp>
-                    <Icons.ArrowDown
-                      size={25}
-                      onClick={() => props.onSortingClick("contect", "desc")}
-                    ></Icons.ArrowDown>
+                    <a href="#">
+                      <Icons.ArrowUp
+                        size={25}
+                        onClick={() => props.onSortingClick("contect", "asc")}
+                      ></Icons.ArrowUp>
+                    </a>
+                    <a href="#">
+                      <Icons.ArrowDown
+                        size={25}
+                        onClick={() => props.onSortingClick("contect", "desc")}
+                      ></Icons.ArrowDown>
+                    </a>
                   </th>
                   <th>
                     BANK
-                    <Icons.ArrowUp
-                      size={25}
-                      onClick={() => props.onSortingClick("bankname", "asc")}
-                    ></Icons.ArrowUp>
-                    <Icons.ArrowDown
-                      size={25}
-                      onClick={() => props.onSortingClick("bankname", "desc")}
-                    ></Icons.ArrowDown>
+                    <a href="#">
+                      <Icons.ArrowUp
+                        size={25}
+                        onClick={() => props.onSortingClick("bankname", "asc")}
+                      ></Icons.ArrowUp>
+                    </a>
+                    <a href="#">
+                      <Icons.ArrowDown
+                        size={25}
+                        onClick={() => props.onSortingClick("bankname", "desc")}
+                      ></Icons.ArrowDown>
+                    </a>
                   </th>
                   <th colSpan="2">Email ID</th>
                   <th>Payment</th>
@@ -125,18 +88,14 @@ const UserTable = props => {
                       </td>
                       <td>
                         <BT.Button variant="link">
-                          {" "}
                           <Icons.Pencil
-                            onClick={() => props.onEditUserClick(userobj)}
+                            onClick={() => props.onEditUserClick(userobj.id)}
                             color="royalblue"
                             size={25}
                           ></Icons.Pencil>
                         </BT.Button>
-
                         {"/"}
-
                         <BT.Button variant="link">
-                          {" "}
                           <Icons.Trash
                             onClick={() => props.onDeleteUserClick(userobj.id)}
                             color="red"
