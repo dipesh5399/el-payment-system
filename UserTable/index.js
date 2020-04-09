4e4b7fbae9c6610f949364f8b670c22a89a7d8ed
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-04-09 19:17:32 +0530

    Add files via upload

--- UserTable/index.js
+++ UserTable/index.js
@@ -1,21 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-import "../UserTable/userTable.css";
+import React, { Component } from "react";
+import Load from "../Loader";
+import "../UserTable/main.css";
 import * as BT from "react-bootstrap";
 import * as Icons from "react-bootstrap-icons";
+import Payment from "../PaymentMethod";
 
-const UserTable = props => {
+class UserTable extends Component {
+  state={
+    modelview: false,
+    ID:"",
+    price:"",
+    cp:"",
+    status:"",
+    selname:""
+  }
+  paymentHandler = (IDEmail,IDName) => {
+            this.setState({
+              modelview:true,
+              ID: IDEmail,
+              selname: IDName
+            })
+  }
+  onChangeHandler = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({ [name]: value,cp:""});
+  };
+  render(){
   var count = 0;
-
-  return props.user.length === 0 ? (
+  var newpr = this.state.price;
+  return this.props.user.length === 0 ? (
     <div align="center">
       <h4>No Data Available.</h4>
     </div>
-  ) : (
-    <BT.Container>
-      <div class="limiter">
-        <div class="table100 ver3 m-b-110 ">
+  ) : (    
+      <div class="limiter"> <hr />  
+         <div class="table100 ver3 m-b-110 ">
           <div class="table100-body ">
             <table
               style={{ tableLayout: "fixed" }}
@@ -29,13 +50,13 @@ const UserTable = props => {
                     <a href="#">
                       <Icons.ArrowUp
                         size={25}
-                        onClick={() => props.onSortingClick("name", "asc")}
+                        onClick={() => this.props.onSortingClick("name", "asc")}
                       ></Icons.ArrowUp>
                     </a>
                     <a href="#">
                       <Icons.ArrowDown
                         size={25}
-                        onClick={() => props.onSortingClick("name", "desc")}
+                        onClick={() => this.props.onSortingClick("name", "desc")}
                       ></Icons.ArrowDown>
                     </a>
                   </th>
@@ -44,28 +65,13 @@ const UserTable = props => {
                     <a href="#">
                       <Icons.ArrowUp
                         size={25}
-                        onClick={() => props.onSortingClick("contect", "asc")}
+                        onClick={() => this.props.onSortingClick("contect", "asc")}
                       ></Icons.ArrowUp>
                     </a>
                     <a href="#">
                       <Icons.ArrowDown
                         size={25}
-                        onClick={() => props.onSortingClick("contect", "desc")}
-                      ></Icons.ArrowDown>
-                    </a>
-                  </th>
-                  <th>
-                    BANK
-                    <a href="#">
-                      <Icons.ArrowUp
-                        size={25}
-                        onClick={() => props.onSortingClick("bankname", "asc")}
-                      ></Icons.ArrowUp>
-                    </a>
-                    <a href="#">
-                      <Icons.ArrowDown
-                        size={25}
-                        onClick={() => props.onSortingClick("bankname", "desc")}
+                        onClick={() => this.props.onSortingClick("contect", "desc")}
                       ></Icons.ArrowDown>
                     </a>
                   </th>
@@ -74,22 +80,23 @@ const UserTable = props => {
                   <th>Profile</th>
                 </tr>
               </thead>
+             { this.props.loader ? <tr style={{textAlignLast: "center"}} ><td colSpan="8"><Load></Load></td></tr> :
               <tbody>
-                {props.user.map((userobj, id) => {
+                {this.props.user.map((userobj, id) => {
                   return (
                     <tr key={id}>
                       <td>{++count}</td>
                       <td>{userobj.name}</td>
                       <td>{userobj.contect}</td>
-                      <td>{userobj.bankname}</td>
+                      
                       <td colSpan="2">{userobj.Email}</td>
                       <td>
-                        <Link to={"/Payment"}>Payment</Link>
+                         <BT.Button variant="link" onClick={() => this.paymentHandler(userobj.Email,userobj.name)}>Charge</BT.Button>
                       </td>
                       <td>
                         <BT.Button variant="link">
                           <Icons.Pencil
-                            onClick={() => props.onEditUserClick(userobj.id)}
+                            onClick={() => this.props.onEditUserClick(userobj.id)}
                             color="royalblue"
                             size={25}
                           ></Icons.Pencil>
@@ -97,7 +104,7 @@ const UserTable = props => {
                         {"/"}
                         <BT.Button variant="link">
                           <Icons.Trash
-                            onClick={() => props.onDeleteUserClick(userobj.id)}
+                            onClick={() => this.props.onDeleteUserClick(userobj.id)}
                             color="red"
                             size={25}
                             alt="Delete Icon"
@@ -107,12 +114,36 @@ const UserTable = props => {
                     </tr>
                   );
                 })}
-              </tbody>
+              </tbody>}
             </table>
           </div>
+          {this.state.modelview &&   <BT.Modal
+          show={true}
+          shouldCloseOnEsc={false}
+          shouldCloseOnOverlayClick={false}
+        > <BT.Modal.Body> <BT.Row>
+        <BT.Col xs="15">
+          <BT.InputGroup>             
+          <div class="input-group-prepend">
+                  <span class="input-group-text" id="basic-addon1">              
+                    Amount
+                  </span>
+                </div>
+              <BT.FormControl
+                placeholder="Enter Amount Here"
+                type="number"
+                value={this.state.price }
+                 onChange={this.onChangeHandler}
+                name="price"
+              ></BT.FormControl>      <BT.Button onClick={() => this.setState({cp: this.state.price})} disabled ={this.state.price === "" ? true : false}>Done</BT.Button>     
+          </BT.InputGroup>
+        </BT.Col></BT.Row></BT.Modal.Body><BT.Modal.Footer>{console.log(newpr)}
+         { this.state.cp !== "" && <Payment email={this.state.ID} name={this.state.selname} p={newpr} updatestatus={this.state.status}/>}
+        <BT.Button size="sm"  variant="secondary" onClick={()=> this.setState({modelview:false,price:"",cp:""})}>Cancle</BT.Button>
+      </BT.Modal.Footer></BT.Modal>}
         </div>
       </div>
-    </BT.Container>
-  );
+    
+  );}
 };
 export default UserTable;
