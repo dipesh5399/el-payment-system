cb7bb440c8c133ad89b34c8aa616ee34b98e0094
dipesh5399 <60647272+dipesh5399@users.noreply.github.com>
2020-04-09 19:15:54 +0530

    Add files via upload

--- component/HomePage/index.js
+++ component/HomePage/index.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import UserTable from "../UserTable/index";
 import SearchInput from "../SearchAndPagiAndAddUser/index";
 import UserAddForm from "../UserAddForm/index";
+import {toast } from "react-toastify";
+import Load from "../Loader";
 import { getUsersAPI, deleteUsersAPI } from "../../ApiServiceProvider";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
 // CardVaultList
 // CardVaultAddEdit isVisible, cardVaultId={1}
 export default class HomePage extends Component {
@@ -16,35 +21,51 @@ export default class HomePage extends Component {
     activePage: 1,
     pageLimit: 5,
     sortBy: "name",
-    sortOrder: "asc"
+    sortOrder: "asc",
+    loading: false,
+    loader : false
   };
   getUsers = () => {
     getUsersAPI(this.state, true).then(fetchusers => {
       this.setState({
         users: fetchusers.items,
-        totalItems: fetchusers.total
+        totalItems: fetchusers.total,
       });
     });
+    setTimeout (() => {
+               
+      this.setState({
+          loader:false
+      })
+  },1000)
   };
   componentDidMount() {
     this.getUsers();
+    this.setState({loading:true})
+           setTimeout (() => {
+               
+               this.setState({
+                   loading:false
+               })
+           },3000)
   }
   pageChangeHandler = (page, limit) => {
     this.setState(
       {
         activePage: page,
-        pageLimit: limit
+        pageLimit: limit,
+        loader:true
       },
       () => {
-        this.getUsers();
-      }
-    );
+        this.getUsers();     
+      }    
+    );    
   };
   onChangeHandler = event => {
     const {
       target: { name, value }
     } = event;
-    this.setState({ [name]: value, activePage: 1 }, () => {
+    this.setState({ [name]: value, activePage: 1,loader:true }, () => {
       this.getUsers();
     });
   };
@@ -52,12 +73,17 @@ export default class HomePage extends Component {
     this.setState(state => ({
       isAdd: true,
       id: userid,
+     
       isDialogVisible: !state.isDialogVisible
     }));
   };
   deleteUserClickHandler = userid => {
     if (window.confirm("Are you sure?") === true) {
       deleteUsersAPI(userid);
+      toast("Successfully Deleted!",{ type: "success" });
+    }
+    else{
+      toast("hello...",{ type: "success" });
     }
     this.getUsers();
   };
@@ -66,7 +92,8 @@ export default class HomePage extends Component {
     this.setState({
       isAdd: false,
       isDialogVisible: false,
-      errorDialog: false
+      errorDialog: false,
+      loader:true
     });
     this.getUsers();
   };
@@ -75,15 +102,19 @@ export default class HomePage extends Component {
       {
         sortBy: attrib,
         sortOrder: order,
-        activePage: 1
+        activePage: 1,loader:true
       },
       () => this.getUsers()
     );
   };
   render() {
-    return (
-      <React.Fragment>
-        <hr />
+    if(this.state.loading){
+      return <div style={{textAlignLast: "center"}}><Load></Load></div>
+      
+    }
+   else { return (
+      <React.Fragment> 
+              
         <SearchInput
           items={Math.ceil(this.state.totalItems / this.state.pageLimit)}
           limit={this.state.pageLimit}
@@ -92,22 +123,23 @@ export default class HomePage extends Component {
           onChange={this.onChangeHandler}
           onAddUserClick={this.newUserButtonClickHandler}
           onPageChange={this.pageChangeHandler}
-        />
-        <hr />
+        />        
         {this.state.isAdd && (
           <UserAddForm
             nameKey={this.state.id}
             onCloseClick={this.closeHandler}
           />
         )}
-        <UserTable
+        {  <UserTable
           user={this.state.users}
           onEditUserClick={this.newUserButtonClickHandler}
           onPaymentCall={this.handlerPayment}
           onDeleteUserClick={this.deleteUserClickHandler}
           onSortingClick={this.sortingHandler}
-        />
+          loader={this.state.loader}
+        />}
       </React.Fragment>
     );
+        }     
   }
 }
